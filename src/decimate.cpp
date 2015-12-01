1c74c00f8db4412dd6550ae4a3853ee057377fb3
Shameek Ganguly <shameekg@stanford.edu>
2015-11-30 16:50:04 -0800

    Added all the decimation stuff

--- src/decimate.cpp
+++ src/decimate.cpp
@@ -1,6 +1,7 @@
 #include <OpenMesh/Core/IO/Options.hh>
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <Eigen/Core>
+#include <Eigen/QR>
 
 #include "decimate.h"
 #include <iostream>
@@ -82,9 +83,48 @@ void intialize(Mesh& _mesh) {
     // INSERT CODE HERE FOR PART 1-------------------------------------------------------------------------------
     // Calculate vertex quadrics from incident triangles
     // ----------------------------------------------------------------------------------------------------------
+    for (Mesh::ConstVertexFaceIter f_it = _mesh.cvf_begin(vh); f_it != _mesh.cvf_end(vh); ++f_it) {
+      const auto fh = *f_it;
+      Eigen::Vector4d fcoeff;
+      // calculate face normal
+      const auto normal = _mesh.calc_face_normal(fh).normalized();
+
+      // calculate face plane coefficients
+      const auto point = _mesh.point(vh);
+      auto d = dot(point, normal);
+      fcoeff << normal[0], normal[1], normal[2], -d;
+
+      // add face quadric contribution to the vertex quadric
+      vertex_quadric(_mesh, vh) += fcoeff * fcoeff.transpose();
+    }
   }
 
   std::cout << "Finished initialization." << std::endl;
+}
+
+// calculate optimal position for a given pair of vertices and associated quadric
+Vector4d optimal_pos(const Mesh& _mesh, const Matrix4d& Q, const VertexHandle& v1, const VertexHandle& v2) {
+  Vector4d ret;
+  Matrix4d s_mat = Q;
+  s_mat.row(3) << 0., 0., 0., 1.;
+  Eigen::ColPivHouseholderQR<Matrix4d> qr;
+  qr.setThreshold(1.1);
+  qr.compute(s_mat);
+  // is s_mat full rank?
+  if (qr.rank() == 4) {
+    Vector4d rhs;
+    rhs << 0., 0., 0., 1.;
+    ret = qr.solve(rhs);
+    ret /= ret[3]; // normalize to w = 1
+    std::cout << "Hello" << std::endl;
+  } else {
+    // handle degenerate cases: naive implementation is to return mid point instead
+    const auto l_pt = _mesh.point(v1);
+    const auto m_pt = 0.5 * (_mesh.point(v1) + _mesh.point(v2));
+    const auto r_pt = _mesh.point(v2);
+    ret << r_pt[0], r_pt[1], r_pt[2], 1.0;
+  }
+  return ret;
 }
 
 double compute_priority(Mesh& _mesh, const Mesh::HalfedgeHandle _heh) {
@@ -94,6 +134,14 @@ double compute_priority(Mesh& _mesh, const Mesh::HalfedgeHandle _heh) {
   // Return priority: The smaller the better
   // Use quadrics to estimate approximation error
   // -------------------------------------------------------------------------------------------------------------
+  const auto vh_f = _mesh.from_vertex_handle(_heh);
+  const auto vh_t = _mesh.to_vertex_handle(_heh);
+  // calculate poor man's quadric
+  const auto Q = vertex_quadric(_mesh, vh_f) + vertex_quadric(_mesh, vh_t);
+  // calculate optimal position to reduce the quadric error
+  const auto new_pos = optimal_pos(_mesh, Q, vh_f, vh_t);
+  // calculate associated error
+  priority = new_pos.transpose() * Q * new_pos;
 
   return priority;
 }
@@ -177,7 +225,7 @@ void enqueue_vertex(Mesh& _mesh, VertexPriorityQueue& _queue,
     // Increase the vertex version and use the updated version for the halfedge
     int& version = vertex_latest_version(_mesh, _vh);
     ++version;
-    //_queue.emplace(_vh, min_heh, min_priority, version);
+    _queue.emplace(_vh, min_heh, min_priority, version);
   }
 }
 
@@ -197,17 +245,46 @@ void decimate(Mesh& _mesh, const unsigned int _target_num_vertices) {
   // INSERT CODE HERE FOR PART 3-----------------------------------------------------------------------------------
   // Decimate using priority queue:
   //
-  // 1) Take first element of queue
-  //  - Check whether the vertex priority is valid using 'is_vertex_priority_valid()'
-  //
-  // 2) Collapse this halfedge
-  //  - Check whether the halfedge collapse is valid using 'is_collapse_valid()'
-  //
-  // 3) Update queue
-  //
+  uint decimate_count = 0;
+  while (num_vertices > _target_num_vertices && !queue.empty()) {
+    // 1) Take first element of queue
+    const auto vp = queue.top();
+    queue.pop();
+
+    //  - Check whether the vertex priority is valid using 'is_vertex_priority_valid()'
+    if (!is_vertex_priority_valid(_mesh, vp)) { continue;}
+
+    // 2) Collapse this halfedge
+    const auto vheh = vp.heh_;
+    //  - Check whether the halfedge collapse is valid using 'is_collapse_valid()'
+    if (!is_collapse_valid(_mesh, vheh)) { continue;}
+
+    // get new vertex location
+    const auto vh_f = _mesh.from_vertex_handle(vheh);
+    const auto vh_t = _mesh.to_vertex_handle(vheh);
+    const auto Q = vertex_quadric(_mesh, vh_f) + vertex_quadric(_mesh, vh_t);
+    const auto new_pos = optimal_pos(_mesh, Q, vh_f, vh_t);
+
+    // update relations to be deleted for the to_vertex
+    _mesh.collapse(vheh); //this collapses the from vertex to the to vertex.
+
+    // change point position
+    // _mesh.set_point(vh_t, Vec3f(new_pos[0], new_pos[1], new_pos[2]));
+
+    // 3) Update queue
+    // enqueue all neighboring vertices including this one
+    enqueue_vertex(_mesh, queue, vh_t);
+    for (Mesh::ConstVertexVertexIter vv_it = _mesh.vv_begin(vh_t); vv_it != _mesh.vv_end(vh_t); ++vv_it) {
+      enqueue_vertex(_mesh, queue, *vv_it);
+    }
+
+    // Delete the items marked to be deleted
+    _mesh.garbage_collection();
+
+    // update num vertices
+    num_vertices = _mesh.n_vertices();
+  }
   // --------------------------------------------------------------------------------------------------------------
 
-  // Delete the items marked to be deleted
-  _mesh.garbage_collection();
   std::cout << "Done." << std::endl;
 }
